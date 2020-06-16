f96844adfb2377f318f2797f869f090b87a21523
Nick Chen <chenyahua2012@hotmail.com>
2020-06-16 17:35:03 -0400

    transfer gulp3 to gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -7,34 +7,58 @@ const cssClean = require('gulp-clean-css');
 
 const browserSync = require('browser-sync').create();
 
-gulp.task('js', function(){
+function js(){
     return gulp.src(['*.js','!gulpfile.js'])
       .pipe(terser())
       .pipe(rename({suffix:'.min'}))
-      .pipe(gulp.dest('dist/'))
-})
+      .pipe(gulp.dest('dist/'));
+}
 
-gulp.task('less', function(){
+function lessStyle(){
     return gulp.src('*.less')
         .pipe(less())
         .pipe(gulp.dest('css/'))
-})
-gulp.task('css',function(){
+}
+// gulp.task('css',['less'],function(){ //css is waiting for it's dependency -> less to compiled before it runs
+//     return gulp.src('css/*.css')
+//         .pipe(concat('build.css'))
+//         .pipe(rename({suffix: '.min'}))
+//         .pipe(cssClean({ compatibility: 'ie*' }))
+//         .pipe(gulp.dest('dist/css/'))
+// })
+
+function css(){
     return gulp.src('css/*.css')
         .pipe(concat('build.css'))
         .pipe(rename({suffix: '.min'}))
-        .pipe(cssClean({ compatibility: 'ie*' }))
+        .pipe(cssClean({ compatibility: 'ie8' }))
         .pipe(gulp.dest('dist/css/'))
-})
+        .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    gulp.watch('*.css', css);
+    gulp.watch('*.less', gulp.series(lessStyle,css));
+    gulp.watch('*.js', js).on('change', browserSync.reload);
+}
 
 // function watch(){
-//     browserSync.init({
-//         server: {
-//             baseDir: './'
-//         }
-//     });
-//     gulp.watch('*.js').on('change', browserSync.reload);
-
+//     gulp.watch('*.js', js);
+//     gulp.watch('*.less', gulp.series(lessStyle,css));
+//     gulp.watch('*.css', css);
 // }
-gulp.task('default', gulp.series('js','less','css'))
-// exports.default = watch;
+
+
+// gulp.task('default', gulp.series(gulp.parallel(js,lessStyle),css));
+// gulp.task('watch', gulp.series(watch, browser_sync))
+
+// exports.jsFile = jsFile;
+// exports.lessStyle = lessStyle;
+// exports.cssStyle = gulp.series(lessStyle,cssStyle);
+exports.default = gulp.series(gulp.parallel(js,lessStyle),css);
+exports.watch = watch;
